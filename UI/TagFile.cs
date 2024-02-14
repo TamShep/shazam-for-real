6a642c8d55d4a5bcd4d32984d4b82ab4ba370af7
Gary <40074095+TamShep@users.noreply.github.com>
2024-02-14 09:36:05 -0500

    Update TagFile.cs

--- UI/TagFile.cs
+++ UI/TagFile.cs
@@ -43,8 +43,8 @@ static class TagFile {
 
             if(result.Success) {
                 Console.WriteLine(result.Url);
-                Console.WriteLine('Artist: '+result.Artist)
-                Console.WriteLine('Title: '+result.Title)
+                Console.WriteLine("Artist: " + result.Artist);
+                Console.WriteLine("Title: " + result.Title);
             } else {
                 Console.WriteLine("-");
             }
